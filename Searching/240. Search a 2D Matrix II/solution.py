a0bfee304ba0416817eb8e597fadec74abb991a4
baxtiyor bekmurodov <baxtiyorbekmurodov@baxtiyors-MacBook-Air.local>
2025-02-10 21:00:32 +0500

    Binary Search

---
+++ Searching/240. Search a 2D Matrix II/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n = len(matrix)
+        m = len(matrix[0])
+
+        r,c = 0, m-1
+
+        while r < n and c >=0:
+            if matrix[r][c] == target:
+                return True
+            if matrix[r][c] < target:
+                r+=1
+            else:
+                c-=1
+        return False
