67f453f5555cc05e799c840d544f29addff12a46
baxtiyor bekmurodov <baxtiyorbekmurodov@baxtiyors-MacBook-Air.local>
2025-02-11 20:29:50 +0500

    Matrix

--- Matrix/2482. Difference Between Ones and Zeros in Row and Column/solution.py
+++ Matrix/2482. Difference Between Ones and Zeros in Row and Column/solution.py
@@ -2,37 +2,16 @@ from typing import List
 
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        ones_row = []
-        ones_col = []
 
-
-        for i in range(len(grid)):
-          count = 0
-   
-          for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-              count+=1
-          ones_row.append(count)
+        ones_row = [sum(row) for row in grid]
+        ones_col = [sum(grid[row][col] for row in range(len(grid))) for col in range(len(grid[0]))]
         
-        for i in range(len(grid[0])):
-            count = 0
-            for row in grid:
-                if row[i] == 1:
-                    count+=1
-            ones_col.append(count)
-            
-         
-        diff = []
-        
+    
         for i in range(len(grid)):
           row = []
-
           for j in range(len(grid[0])):
-            res = ones_row[i] + ones_col[j] - (len(ones_row) - ones_row[i])  - (len(ones_col) - ones_col[j])
-            row.append(res)
-          diff.append(row)
-
-        return diff
+            grid[i][j] = ones_row[i] + ones_col[j] - (len(ones_row) - ones_row[i])  - (len(ones_col) - ones_col[j])
+        return grid
     
 if __name__ == "__main__":
    """ Example 1: """
