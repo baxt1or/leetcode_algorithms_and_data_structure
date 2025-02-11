5bc655ef64ab896727dee1427e52038daaa0e519
baxtiyor bekmurodov <baxtiyorbekmurodov@baxtiyors-MacBook-Air.local>
2025-02-11 13:34:00 +0500

    Arrays

--- Arrays/1064. Fixed Point/solution.py
+++ Arrays/1064. Fixed Point/solution.py
@@ -3,10 +3,20 @@ from typing import List
 class Solution:
     def fixedPoint(self, arr: List[int]) -> int:
 
-        for i in range(len(arr)):
-            if arr[i] == i:
-                return i
-        return -1
+        l, r = 0, len(arr) -1
+        ans = -1
+
+        while l <= r:
+          mid = (l+r) //2
+
+          if arr[mid] == mid:
+            ans = mid
+            r = mid -1
+          elif arr[mid] < mid:
+            l = mid+1
+          else:
+            r = mid -1
+        return ans
 
 if __name__ == "__main__":
     
