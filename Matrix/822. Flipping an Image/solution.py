5c70a40c03afe41a57bb31ae2e7d0c8cef4920b3
baxtiyor bekmurodov <baxtiyorbekmurodov@baxtiyors-MacBook-Air.local>
2025-02-14 19:56:28 +0500

    Matrix

---
+++ Matrix/822. Flipping an Image/solution.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        
+        
+        for row in image:
+          row.reverse()
+        
+        
+        for row in image:
+          for i in range(len(row)):
+            if row[i] == 0:
+              row[i] = 1
+            else:
+              row[i] = 0
+        
+        return image
+
+
+if __name__ == '__main__':
+   
+   """ Example 1: """
+   image = [[1,1,0],[1,0,1],[0,0,0]]
+   print(Solution().flipAndInvertImage(image=image))
+
+
+   """ Example 2: """
+   image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+   print(Solution().flipAndInvertImage(image=image))
