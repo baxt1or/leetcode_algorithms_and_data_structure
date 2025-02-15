2b79f828f84761f6ddfa27c4191ffae97886cf7c
baxtiyor bekmurodov <baxtiyorbekmurodov@baxtiyors-MacBook-Air.local>
2025-02-15 16:02:20 +0500

    Matrix

---
+++ Matrix/2022. Convert 1D Array into 2D Array/solution.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        
+        if m*n != len(original):
+          return []
+
+        grid = [[0]*n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+              idx = i *n +j
+              grid[i][j] = original[idx]
+        
+        return grid
+    
+
+if __name__ == '__main__':
+    
+    """ Example 1: """
+    original = [1,2,3]
+    m =1
+    n =3
+    print(Solution().construct2DArray(original, m, n))
