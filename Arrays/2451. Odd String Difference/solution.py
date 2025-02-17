4f1fefdbe11a942e9bd94ee954d854945a2775bb
baxtiyor bekmurodov <baxtiyorbekmurodov@baxtiyors-MacBook-Air.local>
2025-02-17 21:16:39 +0500

    Arrays

---
+++ Arrays/2451. Odd String Difference/solution.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def oddString(self, words: List[str]) -> str:
+
+        
+        def dff(word):
+            vocab = {chr(i+97):i for i in range(26)}
+            res = []
+
+            for i in range(len(word)-1):
+                diff = vocab[word[i+1]] - vocab[word[i]]
+                res.append(diff)
+            return res
+        
+        seen = []
+        res = []
+
+        diffs = {i:dff(words[i]) for i in range(len(words))}
+
+
+        for val in diffs.values():
+            if val in seen:
+                res.append(val)
+            seen.append(val)
+        
+        return words[[key for key, val in diffs.items() if val not in res][0]]
+
+
+if __name__ == "__main__":
+     
+    """ Example 1: """
+    words = ["dtzca","dtzca","dtzca","yqyyo","dtzca","dtzca"]
+    print(Solution().oddString(words))
