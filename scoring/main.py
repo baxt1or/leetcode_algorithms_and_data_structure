6d27c21d78d30eb24934f498aafb743af3d5bbf3
baxtiyor.b <baxtiyor.b@iman.uz>
2025-07-25 21:49:18 +0500

    Scoring

---
+++ scoring/main.py
@@ -0,0 +1,12 @@
+import pandas
+
+class Solution:
+    def __init__(self, name):
+        self.name = name
+
+
+
+if __name__ == '__main__':
+
+    person = Solution(name="Alex")
+    print(person.name)
